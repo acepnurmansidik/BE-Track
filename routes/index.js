204c8aeb5d1ea456b13d9cc44901c3e6e0d5cde6
acep nurman sidik <acepnurmansidik@gmail.com>
2025-06-02 14:44:30 +0700

    init

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,10 @@ const router = express.Router();
 
 const authRouter = require("../resource/app/auth/router");
 const refparamRouter = require("../resource/app/v1/sys_refparameter/router");
-const financeLedgerRouter = require("../resource/app/v1/sys_financial_ledger/router");
 const AuthorizeUserLogin = require("../resource/middleware/authentification");
 
 router.use("/auth", authRouter);
 router.use(AuthorizeUserLogin);
 router.use("/ref-parameter", refparamRouter);
-router.use("/trx", financeLedgerRouter);
 
 module.exports = router;
